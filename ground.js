124399b6f570bf1a590aafbbd094fd0c461d0d72
HappyCoder10 <69432359+HappyCoder10@users.noreply.github.com>
2021-01-28 08:18:09 -0500

    Add files via upload

--- ground.js
+++ ground.js
@@ -1,20 +1,20 @@
 class Ground{
 
-    constructor(){
+  constructor(){
 
-        var options={
-            isStatic:true,
-            friction:0.4
-        }
-      this.body = Bodies.rectangle(500,625,1200,20,options);
-      World.add(world, this.body);
-    }
-    display(){
+      var options={
+          isStatic:true,
+          friction:0.4
+      }
+    this.body = Bodies.rectangle(500,625,1200,20,options);
+    World.add(world, this.body);
+  }
+  display(){
 
-      rectMode(CENTER);
-      fill("silver");
-      rect(500,625,1200,20);
-    
-    }
+    rectMode(CENTER);
+    fill("silver");
+    rect(500,625,1200,20);
+  
+  }
 
 }
--- mango.js
+++ mango.js
@@ -1,20 +1,21 @@
+
 class Mango{
-    constructor(x, y, diametre) {
-        var options = {
-            isStatic:true,
-            'restitution':0.5,
-            'friction':1.0,
-        }
-        this.body = Bodies.circle(x, y, diametre , options);
-        this.diametre=diametre;
-        this.image = loadImage("mango.png");
-        World.add(world, this.body);
+  constructor(x, y, diametre) {
+      var options = {
+          isStatic:true,
+          'restitution':0.5,
+          'friction':1.0,
       }
-      display(){
-        push();
-        translate(this.body.position.x, this.body.position.y);
-        imageMode(CENTER);
-        image(this.image, 0, 0, this.diametre);
-        pop();
-      }
+      this.body = Bodies.circle(x, y, diametre , options);
+      this.diametre=diametre;
+      this.image = loadImage("mango.png");
+      World.add(world, this.body);
+    }
+    display(){
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.diametre);
+      pop();
+    }
 }
